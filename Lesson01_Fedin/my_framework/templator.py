4b70d8c4a92c076570b7cca3fdb66091f8f72688
Andrey Fedin <afedin@amdocs.com>
2021-05-10 20:03:23 +0300

    Third

--- Lesson01_Fedin/my_framework/templator.py
+++ Lesson01_Fedin/my_framework/templator.py
@@ -1,9 +1,10 @@
-from jinja2 import Template
-import os
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
 
 
 def render(template_name, folder="templates", **kwargs):
-    file_path = os.path.join(folder, template_name)
-    with open(file_path, encoding="utf-8") as file:
-        template = Template(file.read())
+    context = Environment()
+    context.loader = FileSystemLoader(folder)
+    template = context.get_template(template_name)
+    print(template)
     return template.render(**kwargs)
